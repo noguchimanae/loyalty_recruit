8222579d1c07f396d409746a13a632d405e70a3c
野口愛恵 <noguchi@noguchiaimegumi-no-Mac-mini.local>
2016-01-29 20:18:45 +0900

    no message

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -11,13 +11,73 @@ $('.sp_list').on('change', function(){
 
 
 //$(function(){
-//  $('#maximage').maximage({
-//    isBackground: true,
-//    slideShow: true,
-//    slideShowTitle: false,
-//    slideDelay: 5,
-//    overflow: 'auto',
-//    verticalAlign:'top'
+$(function(){
+  $(".articles").each(function(){
+    $(this).find("article:gt(8)").each(function(){$(this).hide();});
+    $(this).append('<div class="panel pt40"><p class="panel_inner button">もっと見る</p></div>');
+    $(this).find("div:last").click(function(){
+      $(this).parent().find("article").show(300); $(this).remove();});
+  });
+});
+
+//$('.label').change(function(){
+//  console.log(3);
+//  if ($('.label:checked').val() == 1) {
+//    // 表示させないから…と思ってまず書いたのはこちら
+//    $('.company').remove();
+//  }
+//});
+
+//$(function(){
+//  $(window).load(function(){
+//    var delaySpeed = 100;
+//    var fadeSpeed = 1000;
+//    $('.articles article').each(function(i){
+//      $(this).delay(i*(delaySpeed)).css({
+//        display:'block',
+//        opacity:'0'
+//      }).animate({
+//        opacity:'1'
+//      },fadeSpeed);
+//    });
 //  });
 //});
 
+//var company = function(){
+//  
+//}
+
+
+$('.index_all').click(function(){
+//  console.log(1);
+  $('.all').animate({
+      opacity: '0',
+      display:'block'
+    }).animate({
+      opacity: '1'
+    })
+});
+
+$('.index_company').click(function(){
+  $('.all').animate({
+    opacity: '0',
+    display: 'block',
+    complete: function(){
+      $('.company').css();
+    }
+  })
+});
+
+
+//$("#animate").click(function(){
+//  $(".block").animate({
+//    width: "toggle",
+//    opacity: "toggle"
+//  },{
+//    duration: "slow",
+//    easing: "linear",
+//     complete: function(){alert("completed!");},
+//     step: function(s){$("#steps").text(s)}
+//    }
+//  );
+//});
--- src/static/js/page.js
+++ src/static/js/page.js
@@ -1,5 +1,17 @@
-
-
 $(".toggle").click(function(){
   $(this).toggleClass("active").next().slideToggle(300);
 });
+
+
+//$('.animation').css('visibility','hidden');
+//$(window).scroll(function(){
+//  var windowHeight = $(window).height(),
+//      topWindow = $(window).scrollTop();
+//  $('.bounce').each(function(){
+//    var targetPosition = $(this).offset().top;
+//    if(topWindow > targetPosition - windowHeight + 100){
+//      $(this).addClass("bounce");
+//    }
+//  });
+//});
+
